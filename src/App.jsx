c1069829401969c77575d3c299f829cb7a612e00
unknown <braynnermarques@hotmail.com>
2023-07-10 12:10:08 -0300

    projeto finalizado, falta apenas o readme

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,7 @@ function App() {
 
         <h2>30-day, hassle-free money back guarantee</h2>
         <div className="top-paragraph">
-          <p>Gain access to our full library of tutorials along with expert code reviews.</p>
-
-          <p>Perfect for any developers who are serious about honing their skills.</p>
+          <p>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
 
         </div>
 
